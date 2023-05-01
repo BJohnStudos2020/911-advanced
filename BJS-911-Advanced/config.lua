180d3fd00de6722d35972f31acd8e9168f920181
BJohnStudos2020 <132253639+BJohnStudos2020@users.noreply.github.com>
2023-05-01 16:48:17 +0930

    Delete BJS-911-Advanced directory

--- BJS-911-Advanced/config.lua
+++
@@ -1,9 +0,0 @@
----------------------------------------------
----- B.John Studios 911 Advanced Script -----
----------------------------------------------
-
-Config = {
-    prefix911 = "^4[911] ^3",
-    prefixduty = "^4[BJS-Duty] ^3",
-    blip_remove_time = 180000                                   -- This is the time in millerseconds that is will take for the 911 blip to go away
-}
