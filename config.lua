89d0311ec8b0bf435622075aba76d1d3704888fa
BJohnStudos2020 <132253639+BJohnStudos2020@users.noreply.github.com>
2023-05-03 15:36:27 +0930

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,6 +3,7 @@
 ---------------------------------------------
 
 Config = {
+    server_name = 'Enter Server Name',
     prefix911 = "^4[911] ^3",
     prefixduty = "^4[BJS-Duty] ^3",
     blip_remove_time = 180000                                   -- This is the time in millerseconds that is will take for the 911 blip to go away
