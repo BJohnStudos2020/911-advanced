0f3b4e8d1795bc19cf19480a0c2e5510e8ed2edd
B.John Studios <132253639+BJohnStudos2020@users.noreply.github.com>
2023-06-23 19:44:30 +0930

    Update config.lua
    Added Script name to config.lua for easy access

--- config.lua
+++ config.lua
@@ -7,5 +7,6 @@ Config = {
     prefix911 = "^4[911] ^3",
     prefixduty = "^4[BJS-Duty] ^3",
     blip_remove_time = 180000,                                   -- This is the time in millerseconds that is will take for the 911 blip to go away
-    perms = false
+    perms = false,
+    script_name = 'BJS-911-Advanced'
 }
