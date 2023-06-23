c656999d7504d5f85fd7c273d7fc7d497eda9eac
B.John Studios <132253639+BJohnStudos2020@users.noreply.github.com>
2023-06-23 20:02:52 +0930

    Update client.lua

--- client.lua
+++ client.lua
@@ -160,7 +160,7 @@ end)
 
 local postals = nil
 Citizen.CreateThread(function()
-    postals = LoadResourceFile(GetCurrentResourceName(), GetResourceMetadata(GetCurrentResourceName(), 'postal_file', 23))
+    postals = LoadResourceFile(GetCurrentResourceName(), GetResourceMetadata(GetCurrentResourceName(), 'postal_file'))
     postals = json.decode(postals)
     for i, postal in ipairs(postals) do postals[i] = { vec(postal.x, postal.y), code = postal.code } end
 end)
